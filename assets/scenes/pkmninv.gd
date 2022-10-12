9ed444c881dc7bf7cf3afd924b47c682dfc949f2
John Parkhurst <jparkhurst120@gmail.com>
2022-10-12 11:29:46 -0400

    50 million assets
    We need to trim once we have what we need

---
+++ assets/scenes/pkmninv.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+"""
+This is where ill setup the script to the palyer to save the current pkmn lineup
+"""
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
