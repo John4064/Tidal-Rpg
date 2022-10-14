0f1de6a51020d32468684c95e8da4a9be338e90f
John Parkhurst <jparkhurst120@gmail.com>
2022-10-14 01:08:05 -0400

    Font + Ui

--- assets/scripts/flowers.gd
+++ assets/scripts/flowers.gd
@@ -4,5 +4,6 @@ extends Area2D
 
 func _on_flowers_body_entered(body):
 	if body.name == "Player":
+		body.health +=15
 		get_tree().queue_delete(self) # Replace with function body
 		#body.score +=1 can access player attributes like this
