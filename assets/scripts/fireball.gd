b56b88d4a1dc0c39411665c8e8d35335cdf26dd7
John Parkhurst <jparkhurst120@gmail.com>
2022-10-22 14:24:53 -0400

    Update fireball.gd

--- assets/scripts/fireball.gd
+++ assets/scripts/fireball.gd
@@ -4,3 +4,9 @@ var tilemap
 var speed = 80
 var direction : Vector2
 var attack_damage
+
+func _ready():
+	tilemap = get_tree().root.get_node("Root/TileMap")
+	
+func _process(delta):
+	position = position + speed * delta * direction
